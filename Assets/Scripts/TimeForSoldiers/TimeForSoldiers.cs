adc01d1c1d26a4b6a674c70259f99156abca603c
samuil.ivanov <samuil2001ivanov@gmail.com>
2018-03-16 16:37:36 +0200

    Timer
    Added a timer for player

---
+++ Assets/Scripts/TimeForSoldiers/TimeForSoldiers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeForSoldiers : MonoBehaviour
+{
+
+    public Text time;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float timeEarned = Time.realtimeSinceStartup;
+        time.text = timeEarned.ToString(".000");
+    }
+}
