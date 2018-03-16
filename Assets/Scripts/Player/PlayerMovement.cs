165d51a02f1e3697a3c5e1b314e19b2b513e398c
samuil.ivanov <samuil2001ivanov@gmail.com>
2018-03-16 10:14:57 +0200

    Player Movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), speed * Time.deltaTime);
+        }
+    }
+}
