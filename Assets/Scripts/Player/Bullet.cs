04d552d5e7841c596cd14de66a800a2499dc12ad
samuil.ivanov <samuil2001ivanov@gmail.com>
2018-03-16 12:11:04 +0200

    Player Shooting

---
+++ Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float MaxScreen;
+    public float shootingSpeed;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position.y += shootingSpeed * Time.deltaTime;
+        transform.position = position;
+        if (transform.position.y > MaxScreen)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+
+    [SerializeField]
+    float ShootingCooldown = 1.0f;
+
+    [SerializeField]
+    GameObject bullet;
+
+    public bool canShoot = true;
+
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        if (canShoot)
+        {
+            GameObject bull = Instantiate(bullet);
+            bull.transform.position = transform.position + new Vector3(0.0f, GetComponent<BoxCollider2D>().size.y / 2 + bull.GetComponent<BoxCollider2D>().size.y / 2, 0.0f);
+            canShoot = false;
+            StartCoroutine(ShootDowntime());
+        }
+    }
+
+    IEnumerator ShootDowntime()
+    {
+        yield return new WaitForSeconds(ShootingCooldown);
+        canShoot = true;
+    }
+}
