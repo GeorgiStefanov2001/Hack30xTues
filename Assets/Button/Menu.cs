3f15201ed46036fbe531ce00c321b8a55979faf4
Vladislav <milenkov48@gmail.com>
2018-03-16 15:17:32 +0200

    Menu
    Added Menu.

---
+++ Assets/Button/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public void Start_Game()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void Quit()
+    {
+        
+    }
+}
