52834bb3ecb758e29bc9454ab53c0b4532e5a642
samuil.ivanov <samuil2001ivanov@gmail.com>
2018-03-16 14:19:17 +0200

    Up Scrolling

---
+++ Assets/Scripts/SideScrolling/BeachDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeachDestroyer : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 beachPosition = transform.position;
+        beachPosition.y -= Camera.main.GetComponent<SideScrolling>().ScrollingSpeed;
+        transform.position = beachPosition;
+        if (transform.position.y < Camera.main.GetComponent<SideScrolling>().BottomBorder)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SideScrolling/SideScrolling.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideScrolling : MonoBehaviour
+{
+    public float BottomBorder, ScrollingSpeed;
+
+    [SerializeField]
+    List<GameObject> waterTiles = new List<GameObject>();
+    Vector3 startPosition = new Vector3();
+
+    void Start()
+    {
+        startPosition = waterTiles[1].transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        Sidescrolling();
+    }
+
+    void Sidescrolling()
+    {
+        for (int i = 1; i < waterTiles.Count; i++)
+        {
+            Vector3 position = waterTiles[i].transform.position;
+            position.y -= ScrollingSpeed * Time.deltaTime;
+            if (waterTiles[i].transform.position.y + waterTiles[i].GetComponent<SpriteRenderer>().size.y / 2 < BottomBorder)
+            {
+                position = startPosition;
+            }
+
+            waterTiles[i].transform.position = position;
+        }
+    }
+}
